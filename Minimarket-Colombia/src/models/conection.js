11d961e93f33d978f841ece7c2f557ff3844df66
Samuel <samuelosro23@gmail.com>
2026-07-21 17:33:57 -0500

    Se crea los datos del clinentes en formato json y se agrega las conectiones del backend

--- Minimarket-Colombia/src/models/conection.js
+++ Minimarket-Colombia/src/models/conection.js
@@ -1,19 +1,73 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import fs from "node:fs/promise";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { randomUUID } from "node:crypto";
 
-// Forzar la carga del .env apuntando a la raíz del proyecto
-dotenv.config();
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(fileURLToPath);
 
-// O si prefieres asegurar los datos de manera directa para evitar fallos de lectura del .env:
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'minimarket_db',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+const router = path.join(dirname, "../data/clientes.js");
 
-console.log("Base de datos conectada correctamente.");
-export default pool;
+async function LeerClientes() {
+  try {
+    const data = await fs.readfile(router, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code == "ENOENT") {
+      await guardarCliente([]);
+      return [];
+    }
+    throw error;
+  }
+}
+
+async function guardarClientes(cliente) {
+  const folder = path.dirname(router);
+
+  await fs.mkdir(folder, { recursive: true });
+
+  await fs.writeFile(router, JSON.stringify(cliente, null, 2), "utf-8");
+}
+
+export async function ListarClientes() {
+  return await LeerClientes();
+}
+
+export async function buscarClientesById(id) {
+  const clientes = await LeerClientes();
+
+  return clientes.find((clientes) => clientes.id == id);
+}
+
+export async function crearCLiente(data) {
+  const clientes = await LeerClientes();
+
+  const nuevoCliente = {
+    id: randomUUID(),
+    nombre: data.nombre.trim(),
+    documento: data.documento.trim(),
+    telefono: data.telefono.trim(),
+    correo: data.correo.trim(),
+    password: data.password.trim(),
+  };
+
+  clientes.push(nuevoCliente);
+
+  await guardarClientes(clientes);
+
+  return nuevoCliente;
+}
+
+export async function actualizarCliente(id, data) {
+  const clientes = await LeerClientes();
+
+  const indice = clientes.findIndex((clientes) => clientes.id === id);
+
+  if (indice === -1) {
+    return null;
+  }
+}
+
+const clienteActual = clientes[indice];
+
+clientes[indice];
