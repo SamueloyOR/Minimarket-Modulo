5d777e87da55eb424f28506df583420e8a568cf1
Samuelosro <samuelosro23@gmail.com>
2026-07-24 14:07:12 -0500

    estructura correcta del proyecto en la eaiz

--- Minimarket-Colombia/src/data/clientes.js
+++ Minimarket-Colombia/src/data/clientes.js
@@ -1,11 +1,20 @@
 [
   {
-    "id": "8db2c563-8af7-4a31-afa1-47dacc05759c",
+    "id": "3c847da4-d249-46ab-ac88-8f55789fe676",
     "nombres": "samuel",
     "apellidos": "rocha",
     "documento": "1010963585",
-    "telefono": "3112646438",
+    "telefono": "2222",
     "correo": "samuelosro23@gmail.com",
+    "password": ""
+  },
+  {
+    "id": "37b0ce19-1822-4aed-abe2-4544d3066531",
+    "nombres": "Alejandro",
+    "apellidos": "Rocha",
+    "documento": "1010963582",
+    "telefono": "3044341572",
+    "correo": "",
     "password": ""
   }
 ]
