a25c67bda7994e718549e83261e1685440d43842
Samuelosro <samuelosro23@gmail.com>
2026-08-19 12:30:28 -0500

    Se corrigen archivos duplicados y se agregan funciones de js basicas

---
+++ Minimarket-Colombia/public/js/auth.js
@@ -0,0 +1,9 @@
+
+function viewPassword(){
+    const input = document.getElementById('password');
+    if(input.type === "password"){
+        input.type = "text"
+    }else{
+        input.type = "password"
+    }
+}
--- Minimarket-Colombia/public/js/login.js
+++
@@ -1,9 +0,0 @@
-
-function viewPassword(){
-    const input = document.getElementById('password');
-    if(input.type === "password"){
-        input.type = "text"
-    }else{
-        input.type = "password"
-    }
-}
